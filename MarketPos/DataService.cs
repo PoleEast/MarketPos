9f82a2750ca95732991bcc06e9682e111763d451
至煌 張簡 <a1525123@gmail.com>
2024-04-30 10:47:00 +0800

    完成將連線功能搬家到ds.cs

--- MarketPos/DataService.cs
+++ MarketPos/DataService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.SqlServer.Server;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Net.Http;
@@ -8,6 +9,10 @@ namespace MarketPos
 {
     internal class DataService
     {
+        public static Dictionary<string, int> categorysDict = [];
+        public static Dictionary<string, int> originsDict = [];
+        public static string ConnString =
+        "Data Source=1.175.113.56,3453;Initial Catalog = dbMarketPos; User ID = MarkPosMan; Password=markpos;";
         private static async Task<bool> DS_TryDS_ConnectionSql(SqlConnection conn)
         {
             try
@@ -22,9 +27,9 @@ namespace MarketPos
             }
         }
 
-        public static async Task<bool> DS_ConnectionSql(string connString)
+        public static async Task<bool> DS_ConnectionSql()
         {
-            using SqlConnection conn = new(connString);
+            using SqlConnection conn = new(ConnString);
             return await DS_TryDS_ConnectionSql(conn);
         }
 
@@ -32,75 +37,106 @@ namespace MarketPos
         public static async Task<List<ProductsData>> DS_getProductCardsData(int first)
         {
             List<ProductsData> products = [];
-            if (await DataService.DS_ConnectionSql(Form1.ConnString))
-            {
-                using SqlConnection conn = new(Form1.ConnString);
-                string sql = @$"SELECT Products.name,Category.name AS  categoryName,price,shelveDate,description,weight,origin.name AS originname,stock
+            if (!await DS_ConnectionSql()) return products;
+            using SqlConnection conn = new(ConnString);
+            string sql = @$"SELECT Products.name,Category.name AS  categoryName,price,shelveDate,description,weight,origin.name AS originname,stock
                                 FROM Products
                                 JOIN Category ON Category.id=category
                                 JOIN origin ON origin.id=origin
                                 ORDER BY Products.id
                                 OFFSET {first} ROWS FETCH NEXT 8 ROWS ONLY";
-                conn.Open();
-                try
+            conn.Open();
+            try
+            {
+                using SqlCommand com = new(sql, conn);
+                await using SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
                 {
-                    using SqlCommand com = new(sql, conn);
-                    using SqlDataReader reader = com.ExecuteReader();
-                    while (reader.Read())
+                    ProductsData card = new()
                     {
-                        ProductsData card = new()
-                        {
-                            Name = (string)reader["name"],
-                            Category = (string)reader["categoryName"],
-                            Description = (string)reader["description"],
-                            Weight = (long)reader["weight"],
-                            Price = (decimal)reader["price"],
-                            ShelveDate = (DateTime)reader["shelveDate"],
-                            Stock = (int)reader["stock"],
-                            Origin = (string)reader["originname"]
-                        };
-                        products.Add(card);
-                    }
-                    return products;
+                        Name = (string)reader["name"],
+                        Category = (string)reader["categoryName"],
+                        Description = (string)reader["description"],
+                        Weight = (long)reader["weight"],
+                        Price = (decimal)reader["price"],
+                        ShelveDate = (DateTime)reader["shelveDate"],
+                        Stock = (int)reader["stock"],
+                        Origin = (string)reader["originname"]
+                    };
+                    products.Add(card);
                 }
-                catch (Exception ex) { MessageBox.Show($"獲取商品資料發生錯誤\n {ex} "); }
+                return products;
             }
+            catch (Exception ex) { MessageBox.Show($"獲取商品資料發生錯誤\n {ex} "); }
+
             return products;
         }
 
-        public async void DS_insertProduct(ProductsData productsData)
+        public static async void DS_insertProduct(ProductsData productsData)
         {
-            if (await DataService.DS_ConnectionSql(Form1.ConnString))
-            {
-
-                using SqlConnection conn = new(Form1.ConnString);
-                string sqlInsert = @"INSERT INTO Products([name],[category],[price],[description],[weight],[origin],[stock])
+            if (!await DS_ConnectionSql()) return;
+            using SqlConnection conn = new(ConnString);
+            string sqlInsert = @"INSERT INTO Products([name],[category],[price],[description],[weight],[origin],[stock])
                                 VALUES(@name,@category,@price,@description,@weight,@origin,@stock)";
-                string sqlSelect = @"SELECT * ,Category.name AS categoryName,Origin.name AS originName
-                                     FROM Products 
-                                     JOIN Category ON Category.id=category
-                                     JOIN Origin ON origin.id=origin
-                                     WHERE Products.name=@name";
-                conn.Open();
-                using SqlCommand comInsert = new(sqlInsert, conn);
-                try
-                {
-                    comInsert.Parameters.AddWithValue("@name", productsData.Name);
-                    comInsert.Parameters.AddWithValue("@category", categorysDict[cbAddP_category.Text]);
-                    comInsert.Parameters.AddWithValue("@price", txbAddP_price.Text.Trim());
-                    comInsert.Parameters.AddWithValue("@description", rtbAddP_description.Text);
-                    comInsert.Parameters.AddWithValue("@weight", txbAddP_weight.Text);
-                    comInsert.Parameters.AddWithValue("@origin", originsDict[cbAddP_origin.Text]);
-                    comInsert.Parameters.AddWithValue("@stock", cbAddP_stock.SelectedIndex);
-                    //執行sql指令
-                    comInsert.ExecuteNonQuery();
+            conn.Open();
+            using SqlCommand comInsert = new(sqlInsert, conn);
+            try
+            {
+                comInsert.Parameters.AddWithValue("@name", productsData.Name);
+                comInsert.Parameters.AddWithValue("@category", categorysDict[productsData.Category]);
+                comInsert.Parameters.AddWithValue("@price", productsData.Price);
+                comInsert.Parameters.AddWithValue("@description", productsData.Description);
+                comInsert.Parameters.AddWithValue("@weight", productsData.Weight);
+                comInsert.Parameters.AddWithValue("@origin", originsDict[productsData.Origin]);
+                comInsert.Parameters.AddWithValue("@stock", productsData.Stock);
+                //執行sql指令
+                comInsert.ExecuteNonQuery();
 
-                    MessageBox.Show($"增加{txbAddP_name.Text}成功");
-                    using SqlCommand comSelect = new(sqlSelect, conn);
-                    comSelect.Parameters.AddWithValue("@name", txbAddP_name.Text);
-                    using SqlDataReader reader = comSelect.ExecuteReader();
-                    reader.Read();
-                    ProductsData card = new()
+                MessageBox.Show($"增加{productsData.Name}成功");
+            }
+
+            catch (Exception ex) { MessageBox.Show($"資料庫寫入錯誤:\n {ex}"); }
+
+        }
+
+        /// <param name="mode">
+        /// 1:利用id搜尋
+        /// 2:利用name搜尋
+        /// </param>
+        public async Task<List<ProductsData>> DS_TSelectProducts<T>(T keyword, int mode)
+        {
+            List<ProductsData> productsDatas = [];
+            if (!await DS_ConnectionSql() || keyword == null) return productsDatas;
+            string sqlSelect = @"SELECT * ,Category.name AS categoryName,Origin.name AS originName
+                                FROM Products 
+                                JOIN Category ON Category.id=category
+                                JOIN Origin ON origin.id=origin";
+            switch (mode)
+            {
+                //id
+                case 1:
+                    if (keyword.GetType() != typeof(int)) return productsDatas;
+                    sqlSelect += @"WHERE Products.id=@keyword";
+                    break;
+                //name
+                case 2:
+                    if (keyword.GetType() != typeof(string)) return productsDatas;
+                    sqlSelect += @"WHERE Products.name=@keyword";
+                    break;
+                default:
+                    return productsDatas;
+            }
+            using SqlConnection conn = new(ConnString);
+            conn.Open();
+            using SqlCommand comInsert = new(sqlSelect, conn);
+            try
+            {
+                using SqlCommand comSelect = new(sqlSelect, conn);
+                comSelect.Parameters.AddWithValue("@keyword", keyword);
+                await using SqlDataReader reader = comSelect.ExecuteReader();
+                while (reader.Read())
+                {
+                    ProductsData data = new()
                     {
                         Name = (string)reader["name"],
                         Category = (string)reader["categoryName"],
@@ -111,10 +147,94 @@ namespace MarketPos
                         Stock = (int)reader["stock"],
                         Origin = (string)reader["originName"]
                     };
-                    setProductCardsData(card);
+                    productsDatas.Add(data);
                 }
+                return productsDatas;
+            }
+            catch { return productsDatas; }
+        }
+        public static async void DS_GetCategoryType()
+        {
+            if (await DS_ConnectionSql())
+            {
+                categorysDict.Clear();
+                using SqlConnection conn = new SqlConnection(ConnString);
+                string sql = @"SELECT * FROM Category";
+                conn.Open();
+                try
+                {
+                    using SqlCommand com = new(sql, conn);
+                    using SqlDataReader reader = com.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        DataService.categorysDict.Add((string)reader["name"], (int)reader["id"]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"cbt獲取商品類型錯誤\n{ex}");
+                }
+            }
+        }
+        public static async void DS_GetOriginType()
+        {
+            if (await DS_ConnectionSql())
+            {
+                originsDict.Clear();
+                using SqlConnection conn = new SqlConnection(ConnString);
+                string sql = @"SELECT * FROM Origin";
+                conn.Open();
+                try
+                {
+                    using SqlCommand com = new(sql, conn);
+                    using SqlDataReader reader = com.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        DataService.originsDict.Add((string)reader["name"], (int)reader["id"]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"cbt獲取商品產地錯誤\n{ex}");
+                }
+            }
+        }
 
+        public static async void DS_AddCategoryType(string category)
+        {
+            if (await DS_ConnectionSql())
+            {
+                using SqlConnection conn = new SqlConnection(ConnString);
+                string sql = @"INSERT INTO Category([name])VALUES(@category)";
+                try
+                {
+                    conn.Open();
+                    using SqlCommand com = new SqlCommand(sql, conn);
+                    com.Parameters.AddWithValue("@category", category);
+                    com.ExecuteNonQuery();
+                    MessageBox.Show($"增加類別 {category} 成功");
+                }
                 catch (Exception ex) { MessageBox.Show($"資料庫寫入錯誤:\n {ex}"); }
+            }
+        }
+
+        public static async void DS_AddOriginType(string origin)
+        {
+            if (await DS_ConnectionSql())
+            {
+                using (SqlConnection conn = new SqlConnection(ConnString))
+                {
+                    string sql = @"INSERT INTO Origin([name])VALUES(@origin)";
+                    try
+                    {
+                        conn.Open();
+                        using SqlCommand com = new SqlCommand(sql, conn);
+                        com.Parameters.AddWithValue("@origin", origin);
+                        com.ExecuteNonQuery();
+                        MessageBox.Show($"增加產地 {origin} 成功");
+                    }
+                    catch (Exception ex) { MessageBox.Show($"資料庫寫入錯誤:\n {ex}"); }
+                }
             }
         }
     }
--- MarketPos/Form1.cs
+++ MarketPos/Form1.cs
@@ -10,12 +10,8 @@ namespace MarketPos
     public partial class Form1 : Form
     {
         public static string Imgpath = @"../../../ProductsImg";
-        public static string ConnString =
-            "Data Source=1.175.113.56,3453;Initial Catalog = dbMarketPos; User ID = MarkPosMan; Password=markpos;";
         private List<ProductCard> productCards = [];
         private List<ProductsData> productsDatas = [];
-        private Dictionary<string, int> categorysDict = [];
-        private Dictionary<string, int> originsDict = [];
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +19,7 @@ namespace MarketPos
         private async void Form1_Load(object sender, EventArgs e)
         {
             //測試資料庫連接
-            await DataService.DS_ConnectionSql(ConnString);
+            await DataService.DS_ConnectionSql();
 
             //抓取介面所有卡片
             productCards.Add(productCard1);
@@ -38,9 +34,9 @@ namespace MarketPos
             getProductCardsData(1);
         }
 
-        private async void setcbPage()
+        private void setcbPage()
         {
-            
+
         }
 
         //獲取商品卡片
@@ -64,7 +60,7 @@ namespace MarketPos
 
         private async void insertProduct()
         {
-            if (await DataService.DS_ConnectionSql(ConnString))
+            if (await DataService.DS_ConnectionSql())
             {
                 ProductsData productData = new ProductsData
                 {
@@ -77,51 +73,6 @@ namespace MarketPos
                     Origin = cbAddP_origin.Text
                 };
                 DataService.DS_insertProduct(productData);
-
-                productsData = new ProductsData();
-                using SqlConnection conn = new(ConnString);
-                string sqlInsert = @"INSERT INTO Products([name],[category],[price],[description],[weight],[origin],[stock])
-                                VALUES(@name,@category,@price,@description,@weight,@origin,@stock)";
-                string sqlSelect = @"SELECT * ,Category.name AS categoryName,Origin.name AS originName
-                                     FROM Products 
-                                     JOIN Category ON Category.id=category
-                                     JOIN Origin ON origin.id=origin
-                                     WHERE Products.name=@name";
-                conn.Open();
-                
-                using SqlCommand comInsert = new(sqlInsert, conn);
-                try
-                {
-                    comInsert.Parameters.AddWithValue("@name", txbAddP_name.Text.Trim());
-                    comInsert.Parameters.AddWithValue("@category", categorysDict[cbAddP_category.Text]);
-                    comInsert.Parameters.AddWithValue("@price", txbAddP_price.Text.Trim());
-                    comInsert.Parameters.AddWithValue("@description", rtbAddP_description.Text);
-                    comInsert.Parameters.AddWithValue("@weight", txbAddP_weight.Text);
-                    comInsert.Parameters.AddWithValue("@origin", originsDict[cbAddP_origin.Text]);
-                    comInsert.Parameters.AddWithValue("@stock", cbAddP_stock.SelectedIndex);
-                    //執行sql指令
-                    comInsert.ExecuteNonQuery();
-
-                    MessageBox.Show($"增加{txbAddP_name.Text}成功");
-                    using SqlCommand comSelect = new(sqlSelect, conn);
-                    comSelect.Parameters.AddWithValue("@name", txbAddP_name.Text);
-                    using SqlDataReader reader = comSelect.ExecuteReader();
-                    reader.Read();
-                    ProductsData card = new()
-                    {
-                        Name = (string)reader["name"],
-                        Category = (string)reader["categoryName"],
-                        Description = (string)reader["description"],
-                        Weight = (long)reader["weight"],
-                        Price = (decimal)reader["price"],
-                        ShelveDate = (DateTime)reader["shelveDate"],
-                        Stock = (int)reader["stock"],
-                        Origin = (string)reader["originName"]
-                    };
-                    setProductCardsData(card);
-                }
-
-                catch (Exception ex) { MessageBox.Show($"資料庫寫入錯誤:\n {ex}"); }
             }
 
         }
@@ -155,66 +106,21 @@ namespace MarketPos
                 cbAddP_stock.Items.Add(i);
         }
 
-        private async void cbAddP_category_HandleCreated(object sender, EventArgs e)
+        private void cbAddP_category_HandleCreated(object sender, EventArgs e)
         {
-            if (await DataService.DS_ConnectionSql(ConnString))
-            {
-                categorysDict.Clear();
-                using SqlConnection conn = new SqlConnection(ConnString);
-                string sql = @"SELECT * FROM Category";
-                conn.Open();
-                try
-                {
-                    using SqlCommand com = new(sql, conn);
-                    using SqlDataReader reader = com.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        categorysDict.Add((string)reader["name"], (int)reader["id"]);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"cbt獲取商品類型錯誤\n{ex}");
-                }
-                cbAddP_category.Items.AddRange(categorysDict.Select(o => o.Key).ToArray());
-            }
+            DataService.DS_GetCategoryType();
+            cbAddP_category.Items.AddRange(DataService.categorysDict.Select(o => o.Key).ToArray());
         }
-        private async void cbAddP_origin_HandleCreated(object sender, EventArgs e)
+        private void cbAddP_origin_HandleCreated(object sender, EventArgs e)
         {
-            if (await DataService.DS_ConnectionSql(ConnString))
-            {
-                originsDict.Clear();
-                using SqlConnection conn = new SqlConnection(ConnString);
-                string sql = @"SELECT * FROM Origin";
-                conn.Open();
-                try
-                {
-                    using SqlCommand com = new(sql, conn);
-                    using SqlDataReader reader = com.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        originsDict.Add((string)reader["name"], (int)reader["id"]);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"cbt獲取商品產地錯誤\n{ex}");
-                }
-                cbAddP_origin.Items.AddRange(originsDict.Select(o => o.Key).ToArray());
-            }
+            DataService.DS_GetOriginType();
+            cbAddP_origin.Items.AddRange(DataService.originsDict.Select(o => o.Key).ToArray());
         }
 
         private void btntest_Click(object sender, EventArgs e)
         {
         }
 
-        private void txbAddP_name_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (txbAddP_name.TextLength > 5)
-            {
-                e.Handled = true;
-            }
-        }
 
         private void txbAddP_price_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -222,7 +128,7 @@ namespace MarketPos
                 e.Handled = true;
         }
 
-        private async void cbAddCategoryType_Click(object sender, EventArgs e)
+        private void cbAddCategoryType_Click(object sender, EventArgs e)
         {
             UserInput userInput = new("請輸入新增的類別:")
             {
@@ -233,24 +139,11 @@ namespace MarketPos
             if (userInput.ShowDialog(this) == DialogResult.Cancel) return;
             if (string.IsNullOrEmpty(userInput.userinput)) return;
 
-            //將新類別輸入資料庫
-            if (await DataService.DS_ConnectionSql(ConnString))
-            {
-                using SqlConnection conn = new SqlConnection(ConnString);
-                string sql = @"INSERT INTO Category([name])VALUES(@category)";
-                try
-                {
-                    conn.Open();
-                    using SqlCommand com = new SqlCommand(sql, conn);
-                    com.Parameters.AddWithValue("@category", userInput.userinput);
-                    com.ExecuteNonQuery();
-                    MessageBox.Show($"增加類別 {userInput.userinput} 成功");
-                }
-                catch (Exception ex) { MessageBox.Show($"資料庫寫入錯誤:\n {ex}"); }
-            }
+            DataService.DS_AddCategoryType(userInput.userinput);
+
         }
 
-        private async void btnAddOriginType_Click(object sender, EventArgs e)
+        private void btnAddOriginType_Click(object sender, EventArgs e)
         {
             UserInput userInput = new("請輸入新增的產地:")
             {
@@ -262,22 +155,7 @@ namespace MarketPos
             if (string.IsNullOrEmpty(userInput.userinput)) return;
 
             //將新類別輸入資料庫
-            if (await DataService.DS_ConnectionSql(ConnString))
-            {
-                using (SqlConnection conn = new SqlConnection(ConnString))
-                {
-                    string sql = @"INSERT INTO Origin([name])VALUES(@origin)";
-                    try
-                    {
-                        conn.Open();
-                        using SqlCommand com = new SqlCommand(sql, conn);
-                        com.Parameters.AddWithValue("@origin", userInput.userinput);
-                        com.ExecuteNonQuery();
-                        MessageBox.Show($"增加產地 {userInput.userinput} 成功");
-                    }
-                    catch (Exception ex) { MessageBox.Show($"資料庫寫入錯誤:\n {ex}"); }
-                }
-            }
+            DataService.DS_AddOriginType(userInput.userinput);
         }
     }
 }
